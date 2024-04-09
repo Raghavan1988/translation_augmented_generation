6450672bc069015a8565a72a9357fd9425153ce8
Raghavan Muthuregunathan <raghavan.mit@gmail.com>
2024-04-08 23:24:49 -0700

    changes to prompt

--- translate_RAG.py
+++ translate_RAG.py
@@ -13,7 +13,123 @@ def translate_to_english(text):
     template = """
          Identify the language of the input input_text: {}
         Output the language, code and english_translation in JSON format using the SCHEMA
-        ...
+        SCHEMA:
+            language: string,
+            input_text: string,
+            code: string,
+            english_translation: string
+
+        If the language matches one of the following in the list, use the code.
+        Language - Code
+    Afrikaans - af
+    Albanian - sq
+    Amharic - am
+    Arabic - ar
+    Armenian - hy
+    Azerbaijani - az
+    Basque - eu
+    Belarusian - be
+    Bengali - bn
+    Bosnian - bs
+    Bulgarian - bg
+    Catalan - ca
+    Cebuano - ceb
+    Chichewa - ny
+    Chinese (Simplified) - zh-cn
+    Chinese (Traditional) - zh-tw
+    Corsican - co
+    Croatian - hr
+    Czech - cs
+    Danish - da
+    Dutch - nl
+    English - en
+    Esperanto - eo
+    Estonian - et
+    Filipino - tl
+    Finnish - fi
+    French - fr
+    Frisian - fy
+    Galician - gl
+    Georgian - ka
+    German - de
+    Greek - el
+    Gujarati - gu
+    Haitian Creole - ht
+    Hausa - ha
+    Hawaiian - haw
+    Hebrew - he
+    Hindi - hi
+    Hmong - hmn
+    Hungarian - hu
+    Icelandic - is
+    Igbo - ig
+    Indonesian - id
+    Irish - ga
+    Italian - it
+    Japanese - ja
+    Javanese - jv
+    Kannada - kn
+    Kazakh - kk
+    Khmer - km
+    Kinyarwanda - rw
+    Korean - ko
+    Kurdish (Kurmanji) - ku
+    Kyrgyz - ky
+    Lao - lo
+    Latin - la
+    Latvian - lv
+    Lithuanian - lt
+    Luxembourgish - lb
+    Macedonian - mk
+    Malagasy - mg
+    Malay - ms
+    Malayalam - ml
+    Maltese - mt
+    Maori - mi
+    Marathi - mr
+    Mongolian - mn
+    Myanmar (Burmese) - my
+    Nepali - ne
+    Norwegian - no
+    Odia - or
+    Pashto - ps
+    Persian - fa
+    Polish - pl
+    Portuguese - pt
+    Punjabi - pa
+    Romanian - ro
+    Russian - ru
+    Samoan - sm
+    Scots Gaelic - gd
+    Serbian - sr
+    Sesotho - st
+    Shona - sn
+    Sindhi - sd
+    Sinhala - si
+    Slovak - sk
+    Slovenian - sl
+    Somali - so
+    Spanish - es
+    Sundanese - su
+    Swahili - sw
+    Swedish - sv
+    Tajik - tg
+    Tamil - ta
+    Tatar - tt
+    Telugu - te
+    Thai - th
+    Turkish - tr
+    Ukrainian - uk
+    Urdu - ur
+    Uyghur - ug
+    Uzbek - uz
+    Vietnamese - vi
+    Welsh - cy
+    Xhosa - xh
+    Yiddish - yi
+    Yoruba - yo
+    Zulu - zu
+
     """
     prompt = template.format(text)  # Format the template with the input text
 
@@ -89,4 +205,4 @@ def my_form_post():
                    "</table>" \
                    "</html>"
 
-    return HTMLRepsonse  # Return the HTML response
+    return HTMLRepsonse  # Return the HTML response 
